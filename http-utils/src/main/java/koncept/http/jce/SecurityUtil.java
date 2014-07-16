9ff3d241dd580f938fe5e89ed0035f05719b5492
nicholas Krul <nicholas.krul@gmail.com>
2014-07-16 23:04:08 +0100

    Refactor and clean up Security Util and auto certificates

--- http-utils/src/main/java/koncept/http/jce/SecurityUtil.java
+++ http-utils/src/main/java/koncept/http/jce/SecurityUtil.java
@@ -1,7 +1,5 @@
 package koncept.http.jce;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -19,120 +17,66 @@ import sun.security.x509.X500Name;
 public class SecurityUtil {
 	private SecurityUtil(){}
 	
-	
-	private static final int keysize = 1024;
-    private static final String commonName = "localhost";
-    private static final String organizationalUnit = "IT";
-    private static final String organization = "test";
-    private static final String city = "London";
-    private static final String state = "none";
-    private static final String country = "UK";
-    private static final long validity = 365; //in days
-    private static final String alias = "kncept";
-    private static final char[] keyPass = "changeit".toCharArray();
-	
-	//https://www.mayrhofer.eu.org/create-x509-certs-in-java
-	public static Object[] makeKeypair() throws Exception {
-		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(1024, new SecureRandom());
-        KeyPair keypair = keyGen.generateKeyPair();
-        PrivateKey privKey = keypair.getPrivate();
-        PublicKey pubKey = keypair.getPublic();
-        return new Object[]{privKey, pubKey};
-	}
-	
 //	//https://www.mayrhofer.eu.org/create-x509-certs-in-java
-//	public void makeCert() {
-//		 Calendar expiry = Calendar.getInstance();
-//	        expiry.add(Calendar.DAY_OF_YEAR, validityDays);
-//	 
-//	        X509Name x509Name = new X509Name("CN=" + dn);
-//
-//	        V3TBSCertificateGenerator certGen = new V3TBSCertificateGenerator();
-//	        certGen.setSerialNumber(new DERInteger(BigInteger.valueOf(System.currentTimeMillis())));
-//	        certGen.setIssuer(PrincipalUtil.getSubjectX509Principal(caCert));
-//	        certGen.setSubject(x509Name);
-//	        DERObjectIdentifier sigOID = X509Util.getAlgorithmOID("SHA1WithRSAEncryption");
-//	        AlgorithmIdentifier sigAlgId = new AlgorithmIdentifier(sigOID, new DERNull());
-//	        certGen.setSignature(sigAlgId);
-//	        certGen.setSubjectPublicKeyInfo(new SubjectPublicKeyInfo((ASN1Sequence)new ASN1InputStream(
-//	                new ByteArrayInputStream(pubKey.getEncoded())).readObject()));
-//	        certGen.setStartDate(new Time(new Date(System.currentTimeMillis())));
-//	        certGen.setEndDate(new Time(expiry.getTime()));
-//	        TBSCertificateStructure tbsCert = certGen.generateTBSCertificate();
-//	}
-	
-//	//http://stackoverflow.com/questions/925377/generate-certificates-public-and-private-keys-with-java
-//	//mmm uses bouncycastle!
-//	public void makeCert() {
-//		RSAPrivateKeySpec serPrivateSpec = new RSAPrivateKeySpec(
-//				/*pub*/BigInteger.valueOf(7), BigInteger.valueOf(/*pri*/(int)5));
-//			fact = KeyFactory.getInstance("RSA");
-//			PrivateKey serverPrivateKey = fact.generatePrivate(serPrivateSpec);
-//
-//			RSAPublicKeySpec serPublicSpec = new RSAPublicKeySpec(
-//			    new BigInteger(agentCL.getSerPubMod()), new BigInteger(agentCL.getSerPubExp()));
-//			PublicKey serverPublicKey = fact.generatePublic(serPublicSpec);
-//
-//			keyStore = KeyStore.getInstance(IMXAgentCL.STORE_TYPE);
-//			keyStore.load(null, SOMEPWD.toCharArray());
-//
-//			Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-//
-//			X509Certificate[] serverChain = new X509Certificate[1];
-//			X509V3CertificateGenerator serverCertGen = new X509V3CertificateGenerator();
-//			X500Principal serverSubjectName = new X500Principal("CN=OrganizationName");
-//			serverCertGen.setSerialNumber(new BigInteger("123456789"));
-//			// X509Certificate caCert=null;
-//			serverCertGen.setIssuerDN(somename);
-//			serverCertGen.setNotBefore(new Date());
-//			serverCertGen.setNotAfter(new Date());
-//			serverCertGen.setSubjectDN(somename);
-//			serverCertGen.setPublicKey(serverPublicKey);
-//			serverCertGen.setSignatureAlgorithm("MD5WithRSA");
-//			// certGen.addExtension(X509Extensions.AuthorityKeyIdentifier, false,new
-//			// AuthorityKeyIdentifierStructure(caCert));
-//			serverCertGen.addExtension(X509Extensions.SubjectKeyIdentifier, false,
-//			    new SubjectKeyIdentifierStructure(serverPublicKey));
-//			serverChain[0] = serverCertGen.generateX509Certificate(serverPrivateKey, "BC"); // note: private key of CA
-//
-//			keyStore.setEntry("xyz",
-//			    new KeyStore.PrivateKeyEntry(serverPrivateKey, serverChain),
-//			    new KeyStore.PasswordProtection("".toCharArray()));
+//	public static Object[] makeKeypair() throws Exception {
+//		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+//        keyGen.initialize(1024, new SecureRandom());
+//        KeyPair keypair = keyGen.generateKeyPair();
+//        PrivateKey privKey = keypair.getPrivate();
+//        PublicKey pubKey = keypair.getPublic();
+//        return new Object[]{privKey, pubKey};
 //	}
 	
 	//http://stackoverflow.com/questions/4634124/how-to-generate-sign-and-import-ssl-certificate-from-java
-	public static KeyStore makeKeyStore() throws Exception {
+	public static KeyStoreDetails makeKeyStore() throws Exception {
+		return makeKeyStore(new CertificateDetails());
+	}
+	
+	public static KeyStoreDetails makeKeyStore(CertificateDetails certificateDetails) throws Exception {
+		if (certificateDetails == null)
+			certificateDetails = new CertificateDetails();
+		else
+			certificateDetails = certificateDetails.clone();
+		
+		KeyStoreDetails ksd = new KeyStoreDetails();
+		ksd.certificateDetails = certificateDetails;
 
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(null, null);
+		ksd.keyStore = KeyStore.getInstance("JKS");
+		ksd.keyStore.load(null, null);
 
         CertAndKeyGen keypair = new CertAndKeyGen("RSA", "SHA1WithRSA", null);
 
-        X500Name x500Name = new X500Name(commonName, organizationalUnit, organization, city, state, country);
+        X500Name x500Name = new X500Name(
+        		certificateDetails.commonName,
+        		certificateDetails.organizationalUnit,
+        		certificateDetails.organization,
+        		certificateDetails.city,
+        		certificateDetails.state,
+        		certificateDetails.country);
 
-        keypair.generate(keysize);
-        PrivateKey privKey = keypair.getPrivateKey();
+        keypair.generate(certificateDetails.keysize);
+        ksd.privateKey = keypair.getPrivateKey();
+        ksd.publicKey = keypair.getPublicKey();
 
         X509Certificate[] chain = new X509Certificate[1];
 
-        chain[0] = keypair.getSelfCertificate(x500Name, new Date(), (long) validity * 24 * 60 * 60);
+        chain[0] = keypair.getSelfCertificate(x500Name, new Date(), (long) certificateDetails.validityDays * 24 * 60 * 60);
 
-        keyStore.setKeyEntry(alias, privKey, keyPass, chain);
+        ksd.keyStore.setKeyEntry(certificateDetails.alias, ksd.privateKey, certificateDetails.keyPass, chain);
 
 //        keyStore.store(new FileOutputStream(".keystore"), keyPass);
-        
-        return keyStore;
+        return ksd;
     }
 	
 	public static SSLContext makeSSLContext() throws Exception {
 		return makeSSLContext(makeKeyStore());
 	}
 	
-	public static SSLContext makeSSLContext(KeyStore ks) throws Exception {
+	public static SSLContext makeSSLContext(KeyStoreDetails ksd) throws Exception {
+		KeyStore ks = ksd.keyStore;
 		
 		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-		kmf.init(ks, SecurityUtil.keyPass);
+		kmf.init(ks, ksd.certificateDetails.keyPass);
 		
 		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
 		tmf.init(ks);
@@ -154,12 +98,34 @@ public class SecurityUtil {
 	
 	
 	
+	public static class CertificateDetails implements Cloneable {
+		public int keysize = 1024;
+		public String commonName = "localhost";
+	    public String organizationalUnit = "IT";
+	    public String organization = "test";
+	    public String city = "London";
+	    public String state = "none";
+	    public String country = "UK";
+	    public long validityDays = 365; //in days
+	    public String alias = "kncept";
+	    public char[] keyPass = "changeit".toCharArray();
+	    
+	    @Override
+	    public CertificateDetails clone() throws CloneNotSupportedException {
+	    	CertificateDetails details = (CertificateDetails)super.clone();
+	    	//deep copy array
+	    	details.keyPass = new char[keyPass.length];
+	    	System.arraycopy(keyPass, 0, details.keyPass, 0, keyPass.length);
+	    	return details;
+	    }
+	}
 	
-	
-	
-	
-	
-	
-	
+	public static class KeyStoreDetails {
+		public CertificateDetails certificateDetails;
+		public PrivateKey privateKey;
+		public PublicKey publicKey;
+		public KeyStore keyStore;
+		
+	}
 	
 }
